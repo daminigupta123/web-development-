ded6b70d72fda4525508f50b221a628b1ed01643
Damini Gupta <damini.cs231079@global.org.in>
2025-09-11 11:25:59 +0530

    add  cart

--- second.js
+++ second.js
@@ -1,13 +1,17 @@
  let inp =document.querySelector('input')
  let btn =document.querySelector('button')
+ let dlt=document.querySelector("delete")
  let ul=document.querySelector("ul")
  btn.addEventListener('click',()=>{
    // console.log("hello")
  let data=  inp.value
  console.log(data);
- 
-let li= document.createElement("li")
-li.innerText="damini"
-ul.append(li)
-
-})
+ let li= document.createElement("li")
+ li.innerText=data
+// li.innerText="damini"
+ ul.append(li)
+ inp.value=" "
+ li.addEventListener("click",()=>{
+    li.remove()
+    })
+ })
