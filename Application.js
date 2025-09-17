75e5ed0ee303fff56bf98e4ea7aa05919e24ffe5
Damini Gupta <damini.cs231079@global.org.in>
2025-09-17 11:22:35 +0530

    api connection

--- Application.js
+++ Application.js
@@ -10,10 +10,22 @@
 // }
 // console.log(a)
 let ul=document.querySelector("ul")
-fetch("https://jsonplaceholder.typicode.com/users").then((data)=>{
+fetch('https://jsonplaceholder.typicode.com/todos/').then((data)=>{
     return data.json()
 })
 .then((res)=>{
     console.log(res);
+    show(res)
 })
+function show(res){
+    for(let i of res){
+       let li=  document.createElement("li")
+         let h4=  document.createElement("h4")
+        li.innerText=i.id
+        h4.innerText=i.tittle
+        ul.append(li,h4)
+        
+    }
+
+}
 
